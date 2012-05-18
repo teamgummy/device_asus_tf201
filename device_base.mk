2fd2c1dc3bb93b0db9d17f133654a0c8425a2ab0
kejar31@kejar31 <jenkins@cyanogenmod.com>
2012-05-17 22:08:43 -0500

    forgot to add this

--- device_base.mk
+++ device_base.mk
@@ -33,10 +33,11 @@ PRODUCT_COPY_FILES += \
 # Ramdisk files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ramdisk/init.cardhu.rc:root/init.cardhu.rc \
+    $(LOCAL_PATH)/ramdisk/init.cardhu.cpu:root/init.cardhu.cpu \
     $(LOCAL_PATH)/ramdisk/init.cardhu.keyboard.rc:root/init.cardhu.keyboard.rc \
     $(LOCAL_PATH)/ramdisk/init.cardhu.usb.rc:root/init.cardhu.usb.rc \
     $(LOCAL_PATH)/ramdisk/ueventd.cardhu.rc:root/ueventd.cardhu.rc \
-	$(LOCAL_PATH)/ramdisk/sbin/keyswap::root/sbin/keyswap
+    $(LOCAL_PATH)/ramdisk/sbin/keyswap::root/sbin/keyswap
 
 # Misc
 PRODUCT_COPY_FILES += \
